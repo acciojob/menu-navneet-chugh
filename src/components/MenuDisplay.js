abf662097d92a4bcda0497657c469374fd3ec62e
navneet-chugh <navneetchugh15@gmail.com>
2023-10-18 14:52:25 +0000

    testing 1

--- src/components/MenuDisplay.js
+++ src/components/MenuDisplay.js
@@ -2,7 +2,7 @@ import React,{useState} from "react";
 import menuItems from "../data/menuItems";
 
 const MenuDisplay = ({selected}) =>{
-    const [dishes,setDishes] = useState(menuItems);
+    // const [dishes,setDishes] = useState(menuItems);
 
     // setDishes(menuItems.filter(menu => menu.category === selected))
     
@@ -11,9 +11,9 @@ const MenuDisplay = ({selected}) =>{
     return(
         <div className="food-card">
            {
-            dishes.map(menu => (
+            menuItems.map(menu => (
                
-                (menu.category === selected || selected=="all") && (
+                (menu.category === selected || selected==="all") && (
                     <div className="food-card-item">
                     <h1>{menu.title}</h1>
                     <p>{menu.price}</p>
@@ -32,5 +32,9 @@ const MenuDisplay = ({selected}) =>{
 }
 
 export default MenuDisplay;
+
+
+
+// a>b && a>c
 
 
