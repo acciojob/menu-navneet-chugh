1ded6ee28aa89cb50f673ed1b17efd2855caface
navneet-chugh <navneetchugh15@gmail.com>
2023-10-18 14:50:49 +0000

    testing

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,22 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React,{useState} from "react";
+import MenuDisplay from "./MenuDisplay";
+import Navbar from "./Navbar";
 
+import "../styles/App.css";
+
+
+
+
+
+const App = () => {
+  const [selected,setSelected] = useState("all");
+   console.log(selected); 
+  return(
+    <div>
+      <Navbar setSelected={setSelected}/>
+      <MenuDisplay selected={selected}/>
+    </div>
+  )
+}
+
+export default App;
